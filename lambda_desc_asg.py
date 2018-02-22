8775dc235055f8492d6dae58da00a70920f1461c
LT <llt@hpe.com>
2018-02-23 00:28:52 +0800

    Update lambda_desc_asg.py

--- lambda_desc_asg.py
+++ lambda_desc_asg.py
@@ -2,14 +2,16 @@ import boto3
 import os
 
 
-
 auto_scaling_grp_name = os.environ['asg_name']
-asg_client = boto3.client('autoscaling')
+
 
 def desc_asg_instance():
+    
+    asg_client = boto3.client('autoscaling')
+    
     response = asg_client.describe_auto_scaling_instances()
     auto_scaling_instances = response['AutoScalingInstances'] #Return a List
-    enum_auto_scaling_instances = list(enumerate(auto_scaling_instances))
+    enum_auto_scaling_instances = list(enumerate(auto_scaling_instances)) #enumerate is python build in function https://docs.python.org/3/library/functions.html#enumerate
     #print(enum_auto_scaling_instances)
     
     total_instances = len(auto_scaling_instances) # count the item in a List
@@ -17,7 +19,6 @@ def desc_asg_instance():
     for index, elem in enum_auto_scaling_instances:
         print(elem['InstanceId'])
         
-
 def lambda_handler(event, context):
     # TODO implement
 
