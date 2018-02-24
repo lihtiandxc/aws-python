dfa69b55811237183c389b3b1ec82c0957bc92b0
LT <llt@hpe.com>
2018-02-25 01:41:05 +0800

    Update lambda_desc_asg.py

--- lambda_desc_asg.py
+++ lambda_desc_asg.py
@@ -4,8 +4,7 @@ import boto3
 asg_name = os.environ['asg_name']
 list_asg_name = asg_name.split(',')
 
-def lambda_handler(event, context):
-    # TODO implement
+def get_ec2_id_from_asg():
     
     asg_client = boto3.client('autoscaling')
     response_asg = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=list_asg_name)
@@ -19,7 +18,18 @@ def lambda_handler(event, context):
         print(asg['AutoScalingGroupName'])
         instances = asg['Instances']
         
+        list_instance = []
+        
         for instance in instances:
-            print(instance['InstanceId'])
+            instance_id = instance['InstanceId']
+            list_instance.append(instance_id)
+        
+        #print(list_instance)
+        return list_instance
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+
 
     return 'Success'
