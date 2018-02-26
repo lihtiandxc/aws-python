47b94f5cd15f4865dc78aefd8363951739ae67e8
LT <llt@hpe.com>
2018-02-27 02:13:48 +0800

    Update main_sg_ec2_attach_checking.py

--- main_sg_ec2_attach_checking.py
+++ main_sg_ec2_attach_checking.py
@@ -1,146 +1,66 @@
-###------- Author : lih-tian.lim@dxc.com
-###------- Version : 1.0
-###------- Title : Function to trigger email notification whenever any security group
-###-------         attach/detach activity on Instances
-
 from datetime import datetime
 import boto3
 import json
-import logging
-import sys
 import os
 
-LOGGER = logging.getLogger()
-for h in LOGGER.handlers:
-    LOGGER.removeHandler(h)
-
-HANDLER = logging.StreamHandler(sys.stdout)
-FORMAT = '%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
-HANDLER.setFormatter(logging.Formatter(FORMAT))
-LOGGER.addHandler(HANDLER)
-LOGGER.setLevel(logging.INFO)
+sns_client = boto3.client('sns')
 
 ###-------Global Var can be defined in Lambda global var and import with os.environ() method
 #str_asg_name = 'limliht-asg,limliht2-asg' #simulate global var in lambda
 #global_asg_name = str_asg_name.split(',')
-service_tag_name = 'Service'
-service_tag = 'account'
-env_tag_name = 'Env'
-env_tag = 'Production'
-country_tag_name = 'Country'
-country_tag_value = 'US'
+
 accountpf_sg_list = []
 sns_topic_arn = ''
 
-###-------Initialize the boto3 sdk
-ec2_resource = boto3.resource('ec2')
-sns_client = boto3.client('sns')
-
-###-------
-def get_instance_id(network_id):
+def construct_sns_msg(e):
     
-    network_interface = ec2_resource.NetworkInterface(network_id)
-    instance_id = network_interface.attachment['InstanceId']
-    result = instance_id
-    return result
+    request_parameters = e['requestParameters']
+    group_id = request_parameters['groupId']
+    ip_permissions = request_parameters['ipPermissions']
+    accesskey_id = e['userIdentity']['accessKeyId']
+    username = e['userIdentity']['userName']
+    event_name = e['eventName']
+    aws_region = e['awsRegion']
+    source_ip = e['sourceIPAddress']
+    event_id = e['eventID']
     
-###-------    
-def get_instance_tag(ec2_id):
-    
-    instance = ec2_resource.Instance(ec2_id)
-    tagging = instance.tags
-    
-    for country_value in tagging:
-        if country_value['Key'] == country_tag_name and country_value['Value'] == country_tag_value:
-            for env_value in tagging:
-                if env_value['Key'] == env_tag_name and env_value['Value'] == env_tag:
-                    for service_value in tagging:
-                        if service_value['Key'] == service_tag_name \
-                        and service_value['Value'] == service_tag :
-                            for name_value in tagging:
-                                if name_value['Key'] == 'Name':
-                                    name_ec2 = name_value['Value']
-                                else:
-                                    pass
-                            
-                            get_instance_tag_result = service_value['Value'].upper() + ' platform EC2' + \
-                            ' with instance id "' +  ec2_id + '" and Instance name "' + name_ec2 + '"'
-                            print(get_instance_tag_result)
-                            return get_instance_tag_result
-                        else:
-                            pass
-                else:
-                    pass
-        else:
-            pass
-
-###-------                    
-def sns_result(e, ec2_details, network_id):
-
-    #Accessing the value of "e" cloudtrail event    
-    details = e['detail']['eventName']
-    accesskey_id =  e['detail']['userIdentity']['accessKeyId']
-    username = e['detail']['userIdentity']['userName']
-    event_id = e['detail']['eventID']
-    aws_region = e['detail']['awsRegion']
-    source_ip =  e['detail']['sourceIPAddress']
-    user_agent = e['detail']['userAgent']
-    parameters = e['detail']['requestParameters']['groupSet']['items']
-    # The iteration method below help to access value of the list
-    sg_parameters_list = [i['groupId'] for i in parameters if 'groupId' in i]
-    sg_parameters = json.dumps(sg_parameters_list)
-    
-    #Looking for illegal securty group
-    illegal_sg = []
-    for sg in sg_parameters_list:
-        if sg not in accountpf_sg_list:
-            illegal_sg.append(sg)
-    
-    #Accessing element time from Event has retuned key error
-    #Solution, transform the Event json to String and re-transform back to json
     str_e = json.dumps(e)
     str_e_data = json.loads(str_e)
-    event_time_json = str_e_data['detail']['eventTime']
+    event_time_json = str_e_data['eventTime']
     #Transform the JSON time format to datetime format
     event_time_datetime_format = str(datetime.strptime(event_time_json, '%Y-%m-%dT%H:%M:%SZ'))
     
-    #Constructing message for SNS    
-    construct_msg = 'Event summary: \
-    \n\nEvent name : ' + details + \
+    rules = json.dumps(ip_permissions['items'])
+    
+    body_msg = 'Event summary: \
+    \n\nEvent name : ' + event_name + \
+    '\nSecurity Group ID : ' + group_id + \
+    '\nChange Items : ' + json.dumps(rules) + \
     '\nEvent Id : ' + event_id + \
     '\nEvent time (UTC) : ' + event_time_datetime_format + \
     '\nUser Access Key : ' + accesskey_id + \
     '\nUsername : ' + username + \
     '\nAWS Region : ' + aws_region + \
     '\nSource IP : ' + source_ip + \
-    '\nAll Security Group attached on this instance : ' + sg_parameters + \
-    '\nNon Account Security Group attached on this instance : ' + json.dumps(illegal_sg) + \
     '\n\n\n' + 'Raw event: ' + \
-    '\n\n' + str_e
+    '\n\n' + str_e 
+    
+    subject_msg = 'Account Platform Security Group ({}) Rules Changes'.format(group_id)
+    trigger_notification(body_msg, subject_msg)
 
-    #Publish SNS topic
-    if details == 'ModifyNetworkInterfaceAttribute':
-        event_json = json.dumps(e)
-        sns_client.publish(TargetArn = sns_topic_arn, MessageStructure = 'string', \
-        Message = construct_msg, Subject = ec2_details)
-    else:
-        pass
+def trigger_notification(event_detail, subject):
+    
+    sns_client.publish(TargetArn = sns_topic_arn, MessageStructure = 'string', \
+    Message = event_detail, Subject = subject)
 
-###-------Main function
+    
 def lambda_handler(event, context):
     # TODO implement
+    print(event)
+    if (event['detail']['eventName'] == 'AuthorizeSecurityGroupIngress' or \
+    'AuthorizeSecurityGroupEgress' or 'RevokeSecurityGroupEgress' or\
+    'RevokeSecurityGroupIngress') and event['detail']['requestParameters'] \
+    ['groupId'] in accountpf_sg_list:
+        result = construct_sns_msg(event['detail'])
     
-    event_network_id = event['detail']['requestParameters']['networkInterfaceId']
-    print(event_network_id)
- 
-    try: 
-        returned_instance_id = get_instance_id(event_network_id)
-        returned_instance_tag = get_instance_tag(returned_instance_id)
-        if returned_instance_tag is not None:
-            sns_result(event, returned_instance_tag, event_network_id)
-        else:
-            pass
-    except  Exception as error:
-        LOGGER.exception(error)
-    
-    return 'Success!'
+    return 'Success'
