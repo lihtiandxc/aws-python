e151cfc5f4190f8f241aec52ce22cdfd4da71575
LT <llt@hpe.com>
2019-03-28 11:50:54 +0800

    Update lambda_warmer.py

--- lambda_warmer.py
+++ lambda_warmer.py
@@ -7,6 +7,6 @@ def lambda_handler(event, context):
     # print(eObjsLoad['Service'])
     try:
         if eObjsLoad['Service'] == 'Warmer':  # configure cloudwatch rule with Rate and and Constant JSON input to have Service as Key and Warmer as Value
-            return False
+            return False # for warming purpose, so to terminate the function ASAP when the service is equal to warmer
     except Exception as error:
-        return False
+        return False # the event JSON object can come in with different Key, so Catch the error and terminate the function
