df7076de0333df532a96e2650c6ad6fee18d894e
LT <llt@hpe.com>
2019-03-28 12:58:08 +0800

    Update lambda_s3ObjectReplicationS3Client.py

--- lambda_s3ObjectReplicationS3Client.py
+++ lambda_s3ObjectReplicationS3Client.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
 
     LogginID = '[' + month+day+hour+min+sec + ']'
     
-    print(LogginID + 'Logging started')
+    print(LogginID,'Logging started')
     
     SourceBucket = 'limliht-pli'
     TargetBucket = 'limliht-pli2'
@@ -47,12 +47,13 @@ def lambda_handler(event, context):
         # print(copy['ResponseMetadata']['HTTPStatusCode'])
         # Sample metadata {'ResponseMetadata': {'RequestId': 'D678C7AE611FA43D', 'HostId': 'rxLOvrP69PgsmesUbNIPWeBivgquDg3Jj1bOwoOZrgolnzJiYviqWIAg4WyajL4ciS4ux/88muc=', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amz-id-2': 'rxLOvrP69PgsmesUbNIPWeBivgquDg3Jj1bOwoOZrgolnzJiYviqWIAg4WyajL4ciS4ux/88muc=', 'x-amz-request-id': 'D678C7AE611FA43D', 'date': 'Thu, 28 Mar 2019 04:23:35 GMT', 'x-amz-copy-source-version-id': 'FTTDsYBYZjSegBYJwoU44spfPuy0r0Ut', 'x-amz-version-id': 'o6CBS4QzWj8GLOrXkWlkm0cMXVdX2qwO', 'content-type': 'application/xml', 'content-length': '234', 'server': 'AmazonS3'}, 'RetryAttempts': 0}, 'CopySourceVersionId': 'FTTDsYBYZjSegBYJwoU44spfPuy0r0Ut', 'VersionId': 'o6CBS4QzWj8GLOrXkWlkm0cMXVdX2qwO', 'CopyObjectResult': {'ETag': '"49f68a5c8493ec2c0bf489821c21fc3b"', 'LastModified': datetime.datetime(2019, 3, 28, 4, 23, 35, tzinfo=tzlocal())}}
         if copy['ResponseMetadata']['HTTPStatusCode'] == 200:
-            print(file + ' Copy operation successed')
+            print(file,'Copy operation successed')
         else:
-            print(file + ' Copy operation failed')
+            print(file,'Copy operation failed')
     print(KeyList)
     
-    print(LogginID + 'Logging ended')
+    print(LogginID,'Logging ended')
     end = time.time()
     TimeSpent = str(round(end-start, 3))
-    print(LogginID + '' + TimeSpent)
+    print(LogginID,TimeSpent)
+
